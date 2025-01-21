28e0fa905111904a32d52045f754b089bbf843e6
riyadhossain565 <hossainriyad565@gmail.com>
2025-01-22 03:35:41 +0600

    setup role and send role data to db

--- src/Pages/Dashboard/AdminDashboard/AllUser/AllUsers.jsx
+++ src/Pages/Dashboard/AdminDashboard/AllUser/AllUsers.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -8,24 +7,60 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import useAxiosSecure from "@/src/Hooks/useAxiosSecure/useAxiosSecure";
 import useUsers from "@/src/Hooks/useUsers/useUsers";
 import { Helmet } from "react-helmet-async";
-import { GrUserWorker } from "react-icons/gr";
 import { FaUserTag } from "react-icons/fa";
+import { GrUserWorker } from "react-icons/gr";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const [users, refetch] = useUsers();
-  console.log(users)
+  const axiosSecure = useAxiosSecure()
+  
 
-
-  const handleRoleChange = async () => {
-    try{
-
-    } 
-    catch (err) {
-      console.log(err)
+  const handleMakeAdmin = async (user) => {
+    try {
+      const res = await axiosSecure.patch(`/users/${user._id}`, {
+        role: "admin", 
+      });
+      // console.log(res.data); 
+      if (res.data.matchedCount > 0) {
+        refetch(); 
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin Now`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    } catch (err) {
+      console.error("Error making admin:", err);
     }
-  }
+  };
+  
+  const handleMakeDeliveryMan = async (user) => {
+    try {
+      const res = await axiosSecure.patch(`/users/${user._id}`, {
+        role: "deliveryMan", 
+      });
+      // console.log(res.data);
+      if (res.data.matchedCount > 0) {
+        refetch(); 
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is a Delivery Man Now`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    } catch (err) {
+      console.error("Error making delivery man:", err);
+    }
+  };
+  
 
   return (
     <div className="p-6">
@@ -72,35 +107,33 @@ const AllUsers = () => {
                   <TableCell className="px-8">{user.totalParcels}</TableCell>
                   <TableCell className="px-8">{user.totalSpent}</TableCell>
                   <TableCell className="px-8">
-
-                    <Button
+                    {
+                      user.role === "deliveryMan" ? (
+                        <p className="font-bold ml-6 text-gray-600">Delivery Man</p>
+                      ) : (
+                        <Button
                       variant="outline"
-                      // className=" "
-                      className={`transition-all ml-6 ${
-                        user.role === "admin"
-                          ? "cursor-not-allowed bg-gray-300"
-                          : "cursor-pointer bg-blue-500/60 hover:bg-blue-600"
-                      }`}
-                      onClick={() => handleRoleChange(user._id, "Delivery Men")}
-                      disabled={user.role === "admin"}
+                      className="bg-blue-500/60 hover:bg-blue-600 transition-all ml-6"
+                      onClick={() => handleMakeDeliveryMan(user)}
+                     
                     >
                       <GrUserWorker className="text-xl" />
                     </Button>
+                      )
+                    }
                   </TableCell>
                   <TableCell className="px-8">
-
-                    <Button
-                      variant="outline"
-                      className={`transition-all ml-6 ${
-                        user.role === "admin"
-                          ? "cursor-not-allowed bg-gray-300"
-                          : "cursor-pointer bg-yellow-500/80 hover:bg-yellow-600"
-                      }`}
-                      onClick={() => handleRoleChange(user._id, "Admin")}
-                      disabled={user.role === "admin"}
-                    >
-                      <FaUserTag className="text-xl" />
-                    </Button>
+                    {user.role === "admin" ? (
+                      <p className="font-bold ml-6 text-gray-600">Admin</p>
+                    ) : (
+                      <Button
+                        variant="outline"
+                        className="hover:bg-yellow-600 bg-yellow-500/80 ml-6"
+                        onClick={() => handleMakeAdmin(user)}
+                      >
+                        <FaUserTag className="text-xl" />
+                      </Button>
+                    )}
                   </TableCell>
                 </TableRow>
               ))}
