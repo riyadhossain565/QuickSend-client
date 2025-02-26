345308ae87119d08df5b183b277b8c4be3dab789
riyadhossain565 <hossainriyad565@gmail.com>
2025-02-26 22:10:56 +0600

    added description

--- src/Pages/Home/DeliverEverything/DeliverEverything.jsx
+++ src/Pages/Home/DeliverEverything/DeliverEverything.jsx
@@ -17,7 +17,7 @@ const DeliverEverything = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 my-10">
-          <div className="bg-[#F0EFE6] px-10 pb-6 text-center transition-all hover:shadow-lg">
+          <div className="bg-[#F0EFE6] px-10 pb-6 text-center rounded-2xl transition-all hover:shadow-lg">
             <div className="w-36 mx-auto py-4">
               <img src={burgImg} alt="" />
             </div>
@@ -25,28 +25,25 @@ const DeliverEverything = () => {
               Food And Drinks
             </h3>
             <p>
-              We ensure your parcels reach their destination in record time
-              without compromising on safety or reliability.
+            Enjoy the freshest food and drinks delivered straight to your doorstep. we ensure fast and reliable delivery to satisfy your cravings anytime, anywhere.
             </p>
           </div>
-          <div className="bg-[#F0EFE6] px-10 pb-6 text-center transition-all hover:shadow-lg">
+          <div className="bg-[#F0EFE6] px-10 pb-6 text-center rounded-2xl transition-all hover:shadow-lg">
             <div className="w-28 mx-auto py-4">
               <img src={boxImg} alt="" />
             </div>
             <h3 className="text-2xl font-bold exo-font mb-4">Packages</h3>
             <p>
-              you can have peace of mind knowing your parcels are safe, secure,
-              and delivered in perfect condition.
+            Safe and secure delivery for all your packages, big or small. Whether it's important documents or special gifts, we ensure timely and reliable shipping to your desired destination.
             </p>
           </div>
-          <div className="bg-[#F0EFE6] px-10 pb-6 text-center transition-all hover:shadow-lg">
+          <div className="bg-[#F0EFE6] px-10 pb-6 text-center rounded-2xl transition-all hover:shadow-lg">
             <div className="w-28 mx-auto py-4">
               <img src={groImg} alt="" />
             </div>
             <h3 className="text-2xl font-bold exo-font mb-4">Groceries</h3>
             <p>
-              Our Service is built on reliability, transparency, and a
-              commitment to excellence.
+            Get fresh groceries delivered to your doorstep with ease. From everyday essentials to organic produce, we provide fast and reliable delivery to keep your kitchen stocked.
             </p>
           </div>
         </div>
